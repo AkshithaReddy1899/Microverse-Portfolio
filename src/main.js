c8e6bd5b7f50fe605db5e69efcb6a3d863927e89
Akshitha Reddy <akshithareddy1899@gmail.com>
2021-12-14 18:44:13 +0530

    Animate menu icon

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const menu = document.getElementById('nav-elements');
+const main = document.getElementById('main');
 
 const checkBox = document.getElementById('toggler');
 
@@ -8,4 +9,22 @@ checkBox.addEventListener('change', function togglerMenu() {
   } else {
     menu.style.opacity = 0;
   }
+});
+
+let anchors = document.querySelectorAll('.active');
+
+console.log(anchors)
+/*
+for(let i=0; i<anchors.length; i++){
+  anchors[i].addEventListener("click", ()=>{
+    main.style.opacity = 1;
+  }) 
+} */
+
+anchors.forEach(link => {
+  link.addEventListener('click', ()=>{
+    menu.style.opacity = 0;
+    main.style.opacity = 1;
+    menu.style.opacity = 0;
+  })
 });
