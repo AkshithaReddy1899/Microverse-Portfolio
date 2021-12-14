c3c710cefbc3b9686bb990259046ff71053d9cd0
Akshitha Reddy <akshithareddy1899@gmail.com>
2021-12-14 15:17:22 +0530

    Add mobile menu

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
-const menu = document.getElementById('nav-elements');
+const menu = document.querySelector('.mobile-nav-elements-container');
+const main = document.getElementById('main');
+const menuItem = document.getElementById('menu-icon');
 
-const checkBox = document.getElementById('toggler');
-
-checkBox.addEventListener('change', function togglerMenu() {
-  if (this.checked) {
-    menu.style.opacity = 1;
-  } else {
-    menu.style.opacity = 0;
+function toggleMenu() {
+  if (menuItem.classList.contains('menu')) {
+    menu.style.display = 'block';
+    menuItem.classList.remove('menu');
+    menuItem.classList.add('close');
+    main.style.opacity = 0;
+  
+  } else if (menuItem.classList.contains('close')) {
+    menu.style.display = 'none';
+    menuItem.classList.remove('close');
+    menuItem.classList.add('menu');
+    main.style.opacity = 1;
   }
-});
+}
+
+menuItem.addEventListener('click', toggleMenu);
